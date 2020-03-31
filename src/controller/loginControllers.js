01086c69dd14ed9c80a95764686661815ce9f9f0
Duc Minh <minh98it@gmail>
2020-03-31 15:07:29 +0700

    add logout

--- src/controller/loginControllers.js
+++ src/controller/loginControllers.js
@@ -11,5 +11,28 @@ module.exports = {
            res.status(401).send(err)
        }
 
+    },
+
+    logout: async (req,res) => {
+        try {
+            req.user.tokens = req.tokens.filter((token) => {
+                return token.token !== req.token
+            })
+            await req.user.save()
+            res.status(200).send("log outed")
+        }catch(e){
+            res.status(500).send()
+        }
+
+    },
+
+    logall: async (erq,res) => {
+        try {
+            req.user.tokens = []
+            await req.user.save()
+            res.send("log outed all")
+        }catch(e){
+            res.status(500).send()
+        }
     }
 }
--- src/routers/login.js
+++ src/routers/login.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const loginController = require('../controller/loginControllers')
+const auth = require('../middleware/auth')
+
 
 router.post('',loginController.post)
+router.post('/logout',auth.auth,loginController.logout)
+router.post('/logall',auth.auth,loginController.logall)
 
 module.exports = router
